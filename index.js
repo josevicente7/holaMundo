85637f902beb877f8ea501b8656ee9b84a57970d
guadalup20 <42047603+guadalup20@users.noreply.github.com>
2018-08-06 13:13:56 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,29 @@
+//Importamos Express tras instalarlo vía NPM
+var express = require('express');
+
+// Definimos App como la función del módulo Express
+var App = express();
+
+// Definimos algunas variables que usaremos en las distintas funciones
+var port = process.env.OPENSHIFT_NODEJS_PORT || 8080
+var options = {
+  root: __dirname
+};
+
+// Definimos funciones para luego usarlas al recibir una petición en el router
+function getHTML(req, res) {
+
+  res.sendFile('./index.html', options, (err) => {
+    if (err) throw err;
+    console.log('Sirviendo index.html')
+  })
+  
+};
+
+// Definimos las rutas
+App.get('/', getHTML);
+
+// Escuchamos el puerto de Express
+App.listen(port, function () {
+  console.log('Aplicacion escuchando en el puerto: ' + port)
+});
